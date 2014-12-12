0e533c6b999bd314db6012bd7ae4f4e7b0d67dbe
Stas Karpov <gilbert_90@mail.ru>
2014-12-12 11:14:57 +0300

    123

--- lib/sidekiq-overlord/server_middleware/status.rb
+++ lib/sidekiq-overlord/server_middleware/status.rb
@@ -13,7 +13,6 @@ module Sidekiq
 
 			def call(worker_class, msg, queue)
 
-				timeout = 0
 				if worker_class.class.try(:overlord?)
 					raise 'Missing options argument' if msg['args'].empty?
 
@@ -28,10 +27,8 @@ module Sidekiq
 					worker_class.spawn_as_overlord(job_namespace)
 					worker_class.expire_time = self.expire
 					worker_class.set_meta(:params, options.to_json)
-					#worker_class.class.sidekiq_options queue: job_namespace
 					worker_class.class.minion.sidekiq_options queue: job_namespace if worker_class.class.minion
 					worker_class.after_spawning if worker_class.respond_to? :after_spawning
-					timeout = self.overlord_timeout
 				elsif worker_class.class.try(:minion?)
 					raise 'Options parameter not found' if msg['args'].first.nil?
 					raise 'Options parameter should be hash' unless msg['args'].first.kind_of? Hash
@@ -41,30 +38,12 @@ module Sidekiq
 					worker_class.expire_time = self.expire
 					worker_class.class.minion.sidekiq_options queue: worker_class.options['job_namespace'] if worker_class.class.minion
 					worker_class.after_spawning if worker_class.respond_to? :after_spawning
-					timeout = self.minion_timeout
 				end
 
-				#if worker_class.has_stop_token?
-				#	return false
-				#end
-
 				begin
-					#if timeout > 0
-					#	Timeout::timeout(timeout) do
-							yield
-						#end
-					#else
-					#	yield
-					#end
-				#rescue ::Sidekiq::Overlord::PauseException
-				#	raise "#{self.name} has no Sidekiq::Overlord::Worker module included" unless worker_class.class.ancestors.include? ::Sidekiq::Overlord::Worker
-				#	#worker_class.set_meta(:paused_time, Time.now.to_i)
-				#	#worker_class.set_meta(:status, 'paused')
-				#	Sidekiq::Overlord.pause_job(worker_class.overlord_jid)
+					yield
 				rescue Exception => ex
 					if worker_class.class.ancestors.include? ::Sidekiq::Overlord::Worker
-						#worker_class.set_meta(:message, ex.message)
-						#worker_class.set_meta(:status, 'error')
 						worker_class.meta_incr(:error)
 						worker_class.save_error_log(ex.message)
 					end
@@ -74,40 +53,19 @@ module Sidekiq
 						worker_class.minion_job_finished(msg['args'][2])
 						worker_class.set_meta(:message, worker_class.finishing_message) if worker_class.respond_to? :finishing_message
 					end
-					#if worker_class.class.try(:overlord?) and worker_class.options.has_key? 'jid'
-					#	`kill -9 #{worker_class.options['pid']}`
-					#end
+					if worker_class.class.try(:overlord?)
+						if worker_class.get_meta(:total).to_i == 0
+							worker_class.set_meta(:message, 'Не было ничего залито')
+							Sidekiq.redis do |conn|
+								conn.decr('jobs:working')
+							end
+							worker_class.finish
+							`kill -9 #{get_meta(:pid)}`
+						end
+					end
 				end
 			ensure
 				if worker_class.class.ancestors.include? ::Sidekiq::Overlord::Worker
-					#if worker_class.class.try(:overlord?)
-					#	if worker_class.get_meta(:total).to_i > 0 && !worker_class.has_stop_token?
-					#		$redis.with do |conn|
-					#			if worker_class.get_meta(:completed_flag) == '1'
-					#				worker_class.delete_meta(:completed_flag)
-					#				worker_class.all_finished if worker_class.respond_to? :all_finished
-					#				worker_class.finish unless worker_class.has_stop_token?
-					#			else
-					#				puts 'Subscribing'
-					#				conn.subscribe "#{worker_class.overlord_jid}:meta" do |on|
-					#					on.subscribe do |channel|
-					#					end
-					#					on.message do |channel, message|
-					#						conn.unsubscribe if %w(paused completed killed).include?(message)
-					#					end
-					#					on.unsubscribe do |channel, subs|
-					#						puts 'unsubscribe'
-					#						worker_class.all_finished if worker_class.respond_to? :all_finished && worker_class.get_meta(:status) != 'killed'
-					#						worker_class.finish unless worker_class.has_stop_token?
-					#					end
-					#				end
-					#			end
-					#
-					#		end
-					#	elsif !worker_class.has_stop_token?
-					#		worker_class.finish
-					#	end
-					#end
 					worker_class.after_work if worker_class.respond_to? :after_work
 					if worker_class.can_kill_process.present?
 						Sidekiq.redis do |conn|
